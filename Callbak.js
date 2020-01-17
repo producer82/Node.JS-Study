a7fc3f3c73e80fe543f0b48148735fcdf018d405
박태원 <producer82@naver.com>
2020-01-17 19:08:14 +0900

    Add files via upload

---
+++ Callbak.js
@@ -0,0 +1,19 @@
+/*
+var fs = require("fs");
+
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log("Program has ended.");
+*/
+
+var fs = require("fs");
+
+fs.readFile("input.txt", 
+    function (err, data){
+        if (err) return console.error(err);
+        console.log(data.toString());
+    }
+);
+
+console.log('program ended');
