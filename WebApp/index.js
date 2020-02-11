0f917bca6946282d8c84883e20aa276db155a696
박태원 <producer82@naver.com>
2020-02-11 16:23:08 +0900

    Update index.js

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,6 +1,35 @@
+//fs.readdir, fs.readFile, url.parse, template ${}, etc...
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+
+function templateHTML(title, list, body){
+	return `
+	<!doctype html>
+	<html>
+		<head>
+			<title>WEB1 - ${title}</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<h1><a href="/">WEB</a></h1>
+			${list}
+			<h2>${title}</h2>
+			${body}
+		</body>
+	</html>
+	`;
+}
+
+function templateList(filelist){
+	var list = '<ul>';
+	for(var i = 0; i < filelist.length; i++){
+		list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+	}
+	list += '</ul>';
+	
+	return list;
+}
 
 http.createServer((req, res) => {
 	var _url = req.url;
@@ -10,70 +39,18 @@ http.createServer((req, res) => {
 
 	if(pathname == '/'){
 		if(queryData.id === undefined){
-			fs.readdir('./Node.JS/WebApp/data', (err, filelist) => {
+			fs.readdir('E:/인생/개발/Node.JS-Study/WebApp/data', (err, filelist) => {
 				var title = 'Welcome';
-				var desc = 'Hello Node.JS';
-				var _list = '<ul>';
-
-				var list = '<ul>';
-				for(var i = 0; i < filelist.length; i++){
-					list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-				}
-				list += '</ul>';
-				
-				/*<ol>
-				<li><a href="/?id=HTML">HTML</a></li>
-				<li><a href="/?id=CSS">CSS</a></li>
-				<li><a href="/?id=JavaScript">JavaScript</a></li>
-				</ol>*/
-				var template = `
-				<!doctype html>
-				<html>
-					<head>
-						<title>WEB1 - ${title}</title>
-						<meta charset="utf-8">
-					</head>
-					<body>
-						<h1><a href="/">WEB</a></h1>
-						${list}
-						<h2>${title}</h2>
-						<p>
-						${desc}
-						</p>
-					</body>
-				</html>
-				`;
+				var template = templateHTML(title, templateList(filelist), 'Hello Node.JS');
 				res.writeHead(200);
 				res.end(template);
 			});
 		}
 		else {
-			fs.readFile(`./Node.JS/WebApp/data/${queryData.id}`, 'utf8', (err, desc) => {
-				fs.readdir('./Node.JS/WebApp/data', (err, filelist) => {
+			fs.readFile(`E:/인생/개발/Node.JS-Study/WebApp/data/${queryData.id}`, 'utf8', (err, desc) => {
+				fs.readdir('E:/인생/개발/Node.JS-Study/WebApp/data', (err, filelist) => {
 					var title = queryData.id
-					var list = '<ul>';
-					for(var i = 0; i < filelist.length; i++){
-						list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-					}
-					list += '</ul>';
-
-					var template = `
-					<!doctype html>
-					<html>
-						<head>
-							<title>WEB1 - ${title}</title>
-							<meta charset="utf-8">
-						</head>
-						<body>
-							<h1><a href="/">WEB</a></h1>
-							${list}
-							<h2>${title}</h2>
-							<p>
-							${desc}
-							</p>
-						</body>
-					</html>
-					`;
+					var template = templateHTML(title, templateList(filelist), desc);
 					res.writeHead(200);
 					res.end(template);
 				});
