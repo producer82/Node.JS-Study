9338758becbd2d7c5d823e989f027066c26457be
박태원 <producer82@naver.com>
2020-02-11 17:26:09 +0900

    Create Title

--- WebApp/index.js
+++ WebApp/index.js
@@ -5,19 +5,20 @@ const url = require("url");
 
 function templateHTML(title, list, body){
 	return `
-	<!doctype html>
-	<html>
-		<head>
-			<title>WEB1 - ${title}</title>
-			<meta charset="utf-8">
-		</head>
-		<body>
-			<h1><a href="/">WEB</a></h1>
-			${list}
-			<h2>${title}</h2>
-			${body}
-		</body>
-	</html>
+		<!doctype html>
+		<html>
+			<head>
+				<title>WEB1 - ${title}</title>
+				<meta charset="utf-8">
+			</head>
+			<body>
+				<h1><a href="/">WEB2</a></h1>
+				${list}
+				<a href="/create">글쓰기</a>
+				<h2>${title}</h2>
+				${body}
+			</body>
+		</html>
 	`;
 }
 
@@ -35,13 +36,15 @@ http.createServer((req, res) => {
 	var _url = req.url;
 	var queryData = url.parse(_url, true).query;
 	var pathname = url.parse(_url, true).pathname;
-	var list;
 
+	console.log(pathname);
 	if(pathname == '/'){
 		if(queryData.id === undefined){
 			fs.readdir('E:/인생/개발/Node.JS-Study/WebApp/data', (err, filelist) => {
 				var title = 'Welcome';
-				var template = templateHTML(title, templateList(filelist), 'Hello Node.JS');
+				var desc = 'Hello Node.JS'
+				var list = templateList(filelist)
+				var template = templateHTML(title, list, desc);
 				res.writeHead(200);
 				res.end(template);
 			});
@@ -56,6 +59,25 @@ http.createServer((req, res) => {
 				});
 			});
 		}
+	}
+	else if(pathname == '/create'){
+		fs.readdir('E:/인생/개발/Node.JS-Study/WebApp/data', (err, filelist) => {
+			var title = 'WEB - create';
+			var list = templateList(filelist)
+			var template = templateHTML(title, list, `
+				<form action="http://localhost:8888/process_create" method="POST">
+					<p><input type="text" name="title" placeholder="제목"></p>
+					<p>
+						<textarea name="desc" placeholder="본문"></textarea>
+					</p>
+					<p>
+						<input type="submit">
+					</p>
+				</form>
+			`);
+			res.writeHead(200);
+			res.end(template);
+		});
 	}
 	else{
 		res.writeHead(404);
