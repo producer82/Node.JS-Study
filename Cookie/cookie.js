d096d5a841fe6631aad243e721a6e60c48a8f35b
박태원 <producer82@naver.com>
2020-03-02 01:20:03 +0900

    이것저것

---
+++ Cookie/cookie.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const cookie = require("cookie");
+
+http.createServer((req, res) => {
+    var cookies = {};
+
+    if(req.headers.cookie !== undefined){
+        cookies = cookie.parse(req.headers.cookie);
+    }
+    
+    console.log(cookies.yummy_cookie);
+    
+    res.writeHead(200, {
+        "Set-Cookie":[
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`
+            `Secure=Secure; Secure`,
+            `HttpOnly=HttpOnly; HttpOnly`
+        ]
+    });
+    res.end("Cookie!!");
+}).listen(8888);
