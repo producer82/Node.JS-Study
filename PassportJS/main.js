dd3ce15053a7ded4c71256269cdcfb2b4b2de13a
박태원 <producer82@naver.com>
2020-03-06 19:22:17 +0900

    PassportJS 2nd day

--- PassportJS/main.js
+++ PassportJS/main.js
@@ -6,9 +6,14 @@ const compression = require('compression');
 const helmet = require('helmet')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
-
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+
+var authData = {
+  email: 'egoing777@gmail.com',
+  password: '111111',
+  nickname: 'egoing'
+}
 
 app.use(helmet());
 app.use(express.static('public'));
@@ -20,6 +25,45 @@ app.use(session({
   saveUninitialized: true,
   store:new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  console.log('serialiseUser', user);
+  done(null, user.email);
+  //done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  console.log('deserialiseUser', id);
+  done(null, authData);
+});
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'pwd'
+  },
+  (username, password, done) => {
+    if(username === authData.email){
+      console.log(1);
+      if(password === authData.password){
+        console.log(2);
+        return done(null, authData);
+      } else {
+        console.log(3);
+        return done(null, false, {
+          message: 'Incorrect password'
+        });
+      }
+    } else {
+      console.log(4);
+      return done(null, false, {
+        message: 'Incorrect username.'
+      });
+    }
+  })
+);
 
 app.post('/auth/login_process', passport.authenticate('local', {
   successRedirect: '/',
